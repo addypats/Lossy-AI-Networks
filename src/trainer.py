3c1720c128d4779404a16450643944a5a124c21a
addypats <adityapat1999@gmail.com>
2025-05-09 16:50:12 -0400

    Added original trainer code to the file with the modified code.

--- src/trainer.py
+++ src/trainer.py
@@ -94,3 +94,37 @@ class MyClassifierCallback(TrainerCallback):
                 break
 
         return super().on_evaluate(args, state, control, **kwargs)
+
+
+
+
+
+
+# def compute_classfication_metrics(eval_pred):
+#     logits, labels = eval_pred
+#     preds = np.argmax(logits, axis=1)
+#     return {
+#         "accuracy": accuracy_score(labels, preds),
+#         "f1": f1_score(labels, preds, average="weighted")
+#     }
+
+# class MyClassifierCallback(TrainerCallback):
+
+#     def __init__(self, args=None):
+#         super().__init__()
+#         self.args = args
+#         self.args['report_ttac'] = sorted(self.args['report_ttac'], reverse=True)
+
+#     def on_evaluate(self, args, state, control, **kwargs):
+        
+#         accuracy = kwargs["metrics"]["eval_accuracy"]
+#         if accuracy > self.args['target_acc']:
+#             print(f"Target accuracy {self.args['target_acc']} reached. Stopping training.")
+#             control.should_training_stop = True
+
+#         for ac in self.args['report_ttac']: # since it is sorted in descending order we only report the last one reached
+#             if accuracy >= ac:
+#                 with open(self.args['report_file'], "a") as f:
+#                     f.write(f"Accuracy: {accuracy:.3f}, Threshold: {ac},  Step: {state.global_step}\n")
+#                 break
+#         return super().on_evaluate(args, state, control, **kwargs)
