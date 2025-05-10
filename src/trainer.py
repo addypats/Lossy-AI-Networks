49ac1685ab5f5e1219d34711876de686babe3fae
addypats <adityapat1999@gmail.com>
2025-05-10 19:49:05 -0400

    Modified trainer.py.

--- src/trainer.py
+++ src/trainer.py
@@ -62,10 +62,13 @@ class DistributedTrainer(Trainer):
 def compute_classfication_metrics(eval_pred):
     logits, labels = eval_pred
     preds = np.argmax(logits, axis=1)
+    acc = accuracy_score(labels, preds)
+    f1 = f1_score(labels, preds, average="weighted")
     return {
-        "accuracy": accuracy_score(labels, preds),
-        "f1": f1_score(labels, preds, average="weighted")
+      "eval_accuracy": acc,
+      "eval_f1": f1
     }
+    
 class MyClassifierCallback(TrainerCallback):
 
     def __init__(self, args=None):
