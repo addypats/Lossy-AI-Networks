d84b1d2a73aada1f3301c901b8a9e26a83a4d8a5
addypats <adityapat1999@gmail.com>
2025-05-25 19:13:00 -0400

    Debugging in progress.

--- run_experiments_tp.sh
+++ run_experiments_tp.sh
@@ -29,7 +29,8 @@ export MASTER_PORT=12355
 export CUDA_VISIBLE_DEVICES=0,1,2,3
 
 # Tensor-parallel world size
-TP_SIZE=(2 4)
+# TP_SIZE=(2 4)
+TP_SIZE=(4)
 
 # Loss-rate grid
 # LOSS_RATES=(0 0.001 0.005 0.01)
@@ -46,20 +47,29 @@ FP_FLAGS=(fp32 fp16)
 mkdir -p output_Llama3.2-1B
 
 for fp_flag in "${FP_FLAGS[@]}"; do
+echo
   # Decide on the actual flag to pass into the Python script
   if [ "$FP_FLAGS" = "fp16" ]; then
     fp_flag="--fp16"
   else
     fp_flag=""   # no flag for fp32
   fi
-  echo "=== Starting with precision ${fp_flag} ==="
+  echo
+  echo "=== Starting with precision ${fp_flag} ===\n"
+  echo
   for tp_size in "${TP_SIZE[@]}"; do
+  echo
     echo "=== Starting tensor parallelism with size ${tp_size} ==="
+    echo
     for dataset in "${DATASETS[@]}"; do
+    echo
       echo "=== Starting with dataset ${dataset} ==="
+      echo
       for loss_rate in "${LOSS_RATES[@]}"; do
         run_id="tp_Llama3.2-1B_precision-${fp_flag}_Num_Nodes-${tp_size}_Data-${dataset}_lr${loss_rate}_batch_size_8"
+        echo
         echo "=== Starting $run_id ==="
+        echo
 
         $TORCHRUN \
           --nproc_per_node $tp_size \
@@ -84,6 +94,7 @@ for fp_flag in "${FP_FLAGS[@]}"; do
             --output_dir           "output_Llama3.2-1B/$run_id" \
 
         echo "=== Completed $run_id ==="
+        echo
         echo
       done
     done
