906b863a1d703d1760d5c7a0173dfa669b7e0c6f
Ubuntu <ubuntu@ip-172-31-90-131.ec2.internal>
2025-06-14 00:06:32 +0000

    8 nodes fp16 long bursts done.

--- run_experiments_tp.sh
+++ run_experiments_tp.sh
@@ -60,8 +60,8 @@ TP_SIZE=(2)
 # GE_CONFIG=("one_precent" "half_percent" "point2_percent")
 # GE_CONFIG=("default" "one")
 # GE_CONFIG=("default" "one_precent" "half_percent")
-GE_CONFIG=("short_1percent" "short_half_percent" "short_point_2percent")
-# GE_CONFIG=("half_percent")
+# GE_CONFIG=("short_1percent" "short_half_percent" "short_point_2percent")
+GE_CONFIG=("point2_percent")
 
 # Loss-rate grid
 LOSS_RATES=(0 0.001 0.005 0.01)
@@ -205,7 +205,7 @@ for iter in "${ITERATIONS[@]}"; do
         echo "=== Starting with dataset ${dataset} ==="
         echo
         for ge_config in "${GE_CONFIG[@]}"; do
-          run_id="tp_gpt2-large_precision-${temp_flag}_Num_Nodes-${tp_size}_ge_config_${ge_config}_Short_Burst_Loss_Iteration_${iter}"
+          run_id="tp_gpt2-large_precision-${temp_flag}_Num_Nodes-${tp_size}_ge_config_${ge_config}_Long_Burst_Loss_Iteration_${iter}"
           echo
           echo "=== Starting $run_id ==="
           echo
