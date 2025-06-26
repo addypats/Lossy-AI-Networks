95ef8eef57667bad68ad3bd58e7896f3a8e71508
Ubuntu <ubuntu@ip-172-31-90-131.ec2.internal>
2025-06-26 01:21:27 +0000

    Short 4 nodes almost all done. 0.001 left. Rest short 2 nodes and long 4 and 2 nodes left.

--- run_experiments_tp.sh
+++ run_experiments_tp.sh
@@ -54,14 +54,14 @@ export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 
 # Tensor-parallel world size
 # TP_SIZE=(2 4 8)
-TP_SIZE=(8)
+TP_SIZE=(4)
 
 # GilbertElliot Loss Model params
 # GE_CONFIG=("default" "one" "one_precent" "half_percent" "point2_percent")
-GE_CONFIG=("one_precent" "half_percent" "point2_percent" "long_point1_percent")
+# GE_CONFIG=("one_precent" "half_percent" "point2_percent" "long_point1_percent")
 # GE_CONFIG=("one_precent" "half_percent" "point2_percent")
 # GE_CONFIG=("default" "one_precent" "half_percent")
-# GE_CONFIG=("short_1percent" "short_half_percent" "short_point_2percent" "short_point1_percent")
+GE_CONFIG=("short_1percent" "short_half_percent" "short_point_2percent" "short_point1_percent")
 # GE_CONFIG=("long_point1_percent")
 # GE_CONFIG=("short_point_2percent" "short_point1_percent")
 
@@ -183,7 +183,7 @@ for iter in "${ITERATIONS[@]}"; do
         echo "=== Starting with dataset ${dataset} ==="
         echo
         for ge_config in "${GE_CONFIG[@]}"; do
-          run_id="tp_gpt2-large_precision-${temp_flag}_Num_Nodes-${tp_size}_ge_config_${ge_config}_Long_Burst_Loss_Iteration_${iter}"
+          run_id="tp_gpt2-large_precision-${temp_flag}_Num_Nodes-${tp_size}_ge_config_${ge_config}_Short_Burst_Loss_Iteration_${iter}"
           echo
           echo "=== Starting $run_id ==="
           echo
