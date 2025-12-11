d42d2a62bb5284db2e63fe96c790c789ff65c776
Aditya Patwardhan <adityapat1999@gmail.com>
2025-12-11 22:43:00 +0000

    sync scripts

--- run_fsdp_test_4gpu_node_0.sh
+++ run_fsdp_test_4gpu_node_0.sh
@@ -31,9 +31,9 @@ LR=1e-5
 #EPOCHS=1
 #EVAL_STEPS=50
 
-CONFIGS=("one_precent" "half_percent" "short_1percent" "short_half_percent")
+# CONFIGS=("one_precent" "half_percent" "short_1percent" "short_half_percent")
 # CONFIGS=("short_1percent" "short_half_percent")
-# CONFIGS=("zero")
+CONFIGS=("half_percent" "short_1percent" "short_half_percent")
 
 
 # CONFIGS_DET=("high_persistence_low_intensity_1" "high_persistence_low_intensity_2" "high_persistence_low_intensity_3" "high_persistence_low_intensity_4" "high_persistence_low_intensity_5" "high_persistence_low_intensity_6" "high_intensity_low_persistence_1" "high_intensity_low_persistence_2" "high_intensity_low_persistence_3" "high_intensity_low_persistence_4" "high_intensity_low_persistence_5" "high_intensity_low_persistence_6")
