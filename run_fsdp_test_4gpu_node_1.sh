60a7b49161eabff4d80f4638aa05a5d6bb57a490
addypats <adityapat1999@gmail.com>
2026-03-12 20:43:08 -0400

    BS 16 LR 0 4 Nodes

--- run_fsdp_test_4gpu_node_1.sh
+++ run_fsdp_test_4gpu_node_1.sh
@@ -28,7 +28,9 @@ GPUS_LIST=(4)
 
 # Per-GPU batch size (HF Trainer interprets this as per_device_* batch size)
 # PER_DEVICE_BS=8
-PER_DEVICE_BS=32
+PER_DEVICE_BS=16
+# PER_DEVICE_BS=24
+# PER_DEVICE_BS=32
 
 LR=1e-5
 #EPOCHS=1
